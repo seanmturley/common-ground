28f233dbb8aa4932ad40a9a53946d8bbccbdaa18
Sean Turley <seanmturley@gmail.com>
2024-08-18 17:48:21 +1200

    [#3] Remove extraneous code and styling from login/page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,7 +5,7 @@ import { redirect } from "next/navigation";
 import { SubmitButton } from "./submit-button";
 
 export default function Login({
-  searchParams,
+  searchParams
 }: {
   searchParams: { message: string };
 }) {
@@ -18,7 +18,7 @@ export default function Login({
 
     const { error } = await supabase.auth.signInWithPassword({
       email,
-      password,
+      password
     });
 
     if (error) {
@@ -40,8 +40,8 @@ export default function Login({
       email,
       password,
       options: {
-        emailRedirectTo: `${origin}/auth/callback`,
-      },
+        emailRedirectTo: `${origin}/auth/callback`
+      }
     });
 
     if (error) {
@@ -52,11 +52,8 @@ export default function Login({
   };
 
   return (
-    <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
-      <Link
-        href="/"
-        className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
-      >
+    <section>
+      <Link href="/">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="24"
@@ -67,53 +64,30 @@ export default function Login({
           strokeWidth="2"
           strokeLinecap="round"
           strokeLinejoin="round"
-          className="mr-2 h-4 w-4 transition-transform group-hover:-translate-x-1"
         >
           <polyline points="15 18 9 12 15 6" />
         </svg>{" "}
         Back
       </Link>
 
-      <form className="flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
-        <label className="text-md" htmlFor="email">
-          Email
-        </label>
+      <form>
+        <label htmlFor="email">Email</label>
+        <input name="email" placeholder="you@example.com" required />
+        <label htmlFor="password">Password</label>
         <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
-          name="email"
-          placeholder="you@example.com"
-          required
-        />
-        <label className="text-md" htmlFor="password">
-          Password
-        </label>
-        <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
           type="password"
           name="password"
           placeholder="••••••••"
           required
         />
-        <SubmitButton
-          formAction={signIn}
-          className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2"
-          pendingText="Signing In..."
-        >
+        <SubmitButton formAction={signIn} pendingText="Signing In...">
           Sign In
         </SubmitButton>
-        <SubmitButton
-          formAction={signUp}
-          className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2"
-          pendingText="Signing Up..."
-        >
+        <SubmitButton formAction={signUp} pendingText="Signing Up...">
           Sign Up
         </SubmitButton>
-        {searchParams?.message && (
-          <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
-            {searchParams.message}
-          </p>
-        )}
+        {searchParams?.message && <p>{searchParams.message}</p>}
       </form>
-    </div>
+    </section>
   );
 }
