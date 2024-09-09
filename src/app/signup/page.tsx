fc327bdf7655b669f97ebe78ada8a148a0c71b0a
Sean Turley <seanmturley@gmail.com>
2024-09-09 23:12:02 +1200

    [#14] Remove the 'back' button from the authentication pages

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -9,23 +9,6 @@ export default function Signup({
 }) {
   return (
     <section>
-      <Link href="/">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <polyline points="15 18 9 12 15 6" />
-        </svg>{" "}
-        Back
-      </Link>
-
       <h1>Sign up</h1>
 
       <form>
