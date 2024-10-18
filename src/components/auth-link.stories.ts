5432cfc95e1a9346d796db22b4009d4e47977ae7
Sean Turley <seanmturley@gmail.com>
2024-10-18 20:55:57 +1300

    [#20] Add 'nextjs.navigation' parameters and new stories to document different use cases of AuthLink

--- src/components/auth-link.stories.ts
+++ src/components/auth-link.stories.ts
@@ -8,4 +8,50 @@ export default {
 
 type Story = StoryObj<typeof AuthLink>;
 
-export const Default: Story = {};
+export const AuthButtonLogIn: Story = {
+  args: {
+    linkPath: "/login"
+  },
+  name: '"Log in" link (in AuthButton)',
+  parameters: {
+    nextjs: {
+      navigation: {
+        pathname: "/articles"
+      }
+    }
+  }
+};
+
+export const AuthFormLogIn: Story = {
+  args: {
+    linkPath: "/login"
+  },
+  name: '"Log in" link (in AuthForm)',
+  parameters: {
+    nextjs: {
+      navigation: {
+        pathname: "/create-account",
+        query: {
+          redirectPath: "/protected"
+        }
+      }
+    }
+  }
+};
+
+export const AuthFormCreateAccount: Story = {
+  args: {
+    linkPath: "/create-account"
+  },
+  name: '"Create account" link (in AuthForm)',
+  parameters: {
+    nextjs: {
+      navigation: {
+        pathname: "/login",
+        query: {
+          redirectPath: "/protected"
+        }
+      }
+    }
+  }
+};
