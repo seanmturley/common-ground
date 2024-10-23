8d57b89b97e19e3983ca8644915aac2c0220df4a
Sean Turley <seanmturley@gmail.com>
2024-10-23 22:51:48 +1300

    [#22] Add AccountId form with SubmitButton that executes addAccountId

--- src/app/(authentication)/account-id/page.tsx
+++ src/app/(authentication)/account-id/page.tsx
@@ -1,5 +1,29 @@
+import styles from "./account-id.module.css";
+import SubmitButton from "@components/submit-button";
+import { addAccountId } from "@utils/auth/user-actions";
+
 export default function AccountId() {
   return (
-    <h1>{"A form for collecting the user's MTGA account ID will go here"}</h1>
+    <form className={styles.form}>
+      <h1>Add MTG Arena account ID</h1>
+
+      <div className={styles.form_field}>
+        <label htmlFor="account-id">Account ID</label>
+        <input
+          className={styles.input}
+          name="account-id"
+          placeholder="DisplayName#12345"
+          type="text"
+          required
+        />
+      </div>
+
+      <SubmitButton
+        formAction={addAccountId}
+        pendingText={"Submitting account ID..."}
+      >
+        Add account ID
+      </SubmitButton>
+    </form>
   );
 }
