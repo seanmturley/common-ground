808365d2c1ff41416c3971d47e4c48b5e8285ac8
Sean Turley <seanmturley@gmail.com>
2024-11-26 21:38:46 +1300

    [#42] Add custom hook with subscription to matchmaking_queue that returns is_matched

---
+++ src/utils/matchmaking/useMatchSubscription.ts
@@ -0,0 +1,40 @@
+import { UUID } from "crypto";
+import { useEffect, useState } from "react";
+import { addBrowserClient } from "@utils/supabase/browser";
+
+export default async function useMatchSubscription() {
+  const [isMatched, setIsMatched] = useState(false);
+
+  const supabase = addBrowserClient();
+
+  const {
+    data: { user }
+  } = await supabase.auth.getUser();
+
+  const id = user?.id as UUID;
+
+  useEffect(() => {
+    const subscription = supabase
+      .channel("matchmaking")
+      .on(
+        "postgres_changes",
+        {
+          event: "UPDATE",
+          schema: "public",
+          table: "matchmaking_queue",
+          filter: `id=eq.${id}`
+        },
+        (payload) => {
+          const updatedData = payload.new;
+          setIsMatched(updatedData.is_matched);
+        }
+      )
+      .subscribe();
+
+    return () => {
+      supabase.removeChannel(subscription);
+    };
+  });
+
+  return isMatched;
+}
