d4d0c0cd45b98900699d27814ff4a86658e05ba0
Sean Turley <seanmturley@gmail.com>
2024-12-16 12:32:12 +1300

    [#63][#59] Replace useGetTimeJoinedQueue with useGetPlayerDatum

--- src/components/queue-timer.tsx
+++ src/components/queue-timer.tsx
@@ -1,20 +1,25 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { useGetTimeJoinedQueue } from "@utils/matchmaking/use-get-time-joined-queue";
 import { formatDuration } from "@utils/matchmaking/format-duration";
+import { useGetPlayerDatum } from "@utils/matchmaking/use-get-player-datum";
 
 export default function QueueTimer() {
   const [timeElapsed, setTimeElapsed] = useState("");
 
-  const timeJoinedQueue = useGetTimeJoinedQueue();
+  const timeJoinedQueue = useGetPlayerDatum({
+    tableName: "queue",
+    columnName: "created_at",
+    initialValue: null
+  });
 
   useEffect(() => {
     if (timeJoinedQueue) {
       const elapsedTimeInterval = setInterval(() => {
         const currentTime = new Date().getTime();
+        const timeJoined = new Date(timeJoinedQueue).getTime();
 
-        const elapsedMilliseconds = currentTime - timeJoinedQueue;
+        const elapsedMilliseconds = currentTime - timeJoined;
 
         // Guard clause for negative time, which produces 59:59
         if (elapsedMilliseconds < 0) return;
