08063530b10e6e57340b94cde6a9898e8bd1faef
Sean Turley <seanmturley@gmail.com>
2024-12-17 22:54:14 +1300

    [#62] Change declineMatchAction to use new decline_match PostgresSQL function

--- src/utils/matchmaking/decline-match-action.ts
+++ src/utils/matchmaking/decline-match-action.ts
@@ -1,17 +1,46 @@
 "use server";
 
-import { removePlayerFromQueue } from "@utils/matchmaking/remove-player-from-queue";
+import type { UUID } from "crypto";
+import { redirect } from "next/navigation";
+import { getCurrentUser } from "@utils/auth/get-current-user";
+import { isValidUuid } from "@utils/matchmaking/data-validation";
+import { addServerClient } from "@utils/supabase/server";
 
 export const declineMatchAction: FormAction = async function (
   prevState,
   formData
 ) {
-  const { message } = await removePlayerFromQueue({
-    preserve_match: false
-  });
+  const supabase = await addServerClient();
+  const { isAuthenticated, user } = await getCurrentUser(supabase);
+
+  if (!isAuthenticated) {
+    redirect("/login");
+  }
+
+  const current_player_id = user?.id as UUID;
+
+  if (!isValidUuid(current_player_id)) {
+    return {
+      ...prevState,
+      message: "Invalid player ID."
+    };
+  }
+
+  const { error } = await supabase
+    .rpc("decline_match", {
+      current_player_id
+    })
+    .single();
+
+  if (error) {
+    console.error(`Error declining the match: ${error.message}`);
+    return {
+      ...prevState,
+      message: "Error declining the match."
+    };
+  }
 
   return {
-    ...prevState,
-    message
+    ...prevState
   };
 };
