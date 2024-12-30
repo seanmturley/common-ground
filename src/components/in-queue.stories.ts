bab85edfbd75d6b0ae83e4758d7049177fbc3851
Sean Turley <seanmturley@gmail.com>
2024-12-30 23:19:01 +1300

    [#59] Add basic stories file for InQueue

---
+++ src/components/in-queue.stories.ts
@@ -0,0 +1,11 @@
+import InQueue from "@components/in-queue";
+import type { Meta, StoryObj } from "@storybook/react";
+
+export default {
+  title: "InQueue",
+  component: InQueue
+} satisfies Meta<typeof InQueue>;
+
+type Story = StoryObj<typeof InQueue>;
+
+export const Default: Story = {};
