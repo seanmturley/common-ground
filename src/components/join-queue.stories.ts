daa979530e26447a727dd60be70cd5d1aa39a904
Sean Turley <seanmturley@gmail.com>
2024-12-30 23:16:13 +1300

    [#41] Add basic stories file for JoinQueue

---
+++ src/components/join-queue.stories.ts
@@ -0,0 +1,11 @@
+import JoinQueue from "@components/join-queue";
+import type { Meta, StoryObj } from "@storybook/react";
+
+export default {
+  title: "JoinQueue",
+  component: JoinQueue
+} satisfies Meta<typeof JoinQueue>;
+
+type Story = StoryObj<typeof JoinQueue>;
+
+export const Default: Story = {};
